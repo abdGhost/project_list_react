a4638216fa43ff813788ad3e828f3a335e173c41
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2024-02-09 13:17:29 +0530

    added project list in sidebar dynamically

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -39,9 +39,9 @@ export default function NewProject({ onAdd }) {
       </menu>
 
       <div>
-        <Input ref={titleRef} label="Title" />
-        <Input ref={descriptionRef} label="Description" textarea />
-        <Input ref={dueDateRef} label="Due Date" />
+        <Input type="text" ref={titleRef} label="Title" />
+        <Input type="text" ref={descriptionRef} label="Description" textarea />
+        <Input type="date" ref={dueDateRef} label="Due Date" />
       </div>
     </div>
   );
